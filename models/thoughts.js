c3aaafbf1affd82adf28addccada7a6f8f5152c3
Alice Bryer <alicebryer123@gmail.com>
2022-09-07 17:36:30 +0100

    thoughts model done

--- models/thoughts.js
+++ models/thoughts.js
@@ -1,1 +1,19 @@
 const { Schema, model } = require("mongoose");
+
+const thoughtsSchema = new Schema({
+  thoughtText: {
+    type: String,
+    require: true,
+    max_length: 280,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [reactionSchema],
+});
--- models/user.js
+++ models/user.js
@@ -1,45 +1,30 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const userSchema = new Schema (
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    // add valid email address property here.
+  },
+  thoughts: [
     {
-        username: {
-            type:String,
-            unique:true,
-            required:true,
-            trim:true,
-        }, 
-        email: {
-            type:String,
-            required:true,
-            unique:true,
-            // add valid email address property here.
-          },
-        thoughts: [
-            {
-                type:Schema.Types.ObjectId,
-                ref: 'thought_id',
-            },
-        ],
-        friends: [
-            {
-                type:Schema.Types.ObjectId,
-                ref:User
-            },
-        ],
+      type: Schema.Types.ObjectId,
+      ref: "thought_id",
     },
-)
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: User,
+    },
+  ],
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-Schema Settings:
-Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+module.exports = User;
