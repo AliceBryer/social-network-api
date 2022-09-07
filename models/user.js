55b070e61b080dd6dfee1be6db7c46fd4da33150
Alice Bryer <alicebryer123@gmail.com>
2022-09-07 17:29:43 +0100

    user model mostly set up

--- models/user.js
+++ models/user.js
@@ -7,37 +7,37 @@ const userSchema = new Schema (
             unique:true,
             required:true,
             trim:true,
-        }
-    }
+        }, 
+        email: {
+            type:String,
+            required:true,
+            unique:true,
+            // add valid email address property here.
+          },
+        thoughts: [
+            {
+                type:Schema.Types.ObjectId,
+                ref: 'thought_id',
+            },
+        ],
+        friends: [
+            {
+                type:Schema.Types.ObjectId,
+                ref:User
+            },
+        ],
+    },
 )
 
-username
-
-String
-Unique
-Required
-Trimmed
 
 
 
-email
-
-String
-Required
-Unique
-Must match a valid email address (look into Mongoose's matching validation)
 
 
 
-thoughts
-
-Array of _id values referencing the Thought model
 
 
 
-friends
-
-Array of _id values referencing the User model (self-reference)
 
 
 
