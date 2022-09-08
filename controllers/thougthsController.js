d4f916f5da0a8302f12731a1614802f2862aaa96
Alice Bryer <alicebryer123@gmail.com>
2022-09-08 12:55:40 +0100

    added thought controller

--- controllers/thougthsController.js
+++ controllers/thougthsController.js
@@ -1,2 +1,33 @@
 const { thoughts } = require("../models/thoughts");
 module.exports = thoughtsController;
+
+// Create thought
+// Get all thoughts
+
+const thoughtController = {
+    getAllThoughts(req, res) {
+        Thought.findAll({})
+            
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+        // get one user by id
+        getThoughtById({ params }, res) {
+            Thought.findOne({ _id: params.thoughtId })
+                .then(dbThoughtData => {
+                    if (!dbThoughtData) {
+                        res.status(404).json({ message: 'No id matched, please try again' });
+                        return;
+                    }
+                    res.json(dbThoughtData);
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.status(400).json(err);
+                });
+        },
+    
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,2 +1,10 @@
 const { User } = require("../models/user");
 module.exports = UserController;
+
+// Delete User
+// Update User
+// Find ALL Users
+// Find user by Id
+// Create user
+// Create second user
+// Create third user
