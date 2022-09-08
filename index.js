2afbca59f849c65550dc1dd4312e411004e3a83b
Alice Bryer <alicebryer123@gmail.com>
2022-09-08 13:34:01 +0100

    put together index.js

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-const mongoose = require("mongoose");
 const express = require("express");
+const routes = require("./routes");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(require(routes));
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
